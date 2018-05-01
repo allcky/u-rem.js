bbcf8720e5870a8a198aa838b115ee4f42fb0ced
Allcky <allcky@qq.com>
2018-05-01 22:00:31 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # u-rem.js
 This is a mobile end layout Library.
+
+[![npm version](https://img.shields.io/npm/v/u-rem.js.svg?style=flat-square)](https://www.npmjs.org/package/u-rem.js)
+[![npm downloads](https://img.shields.io/npm/dm/u-rem.js.svg?style=flat-square)](http://npm-stat.com/charts.html?package=u-rem.js)
+
+## 安装(Install)
+```
+npm install --save u-rem.js
+```
+## 下载(Download)
+https://github.com/allcky/u-rem.js/releases/download/v1.0.0/u-rem.min.js
+
+https://github.com/allcky/u-rem.js/releases/download/v1.0.0/u-rem.js
+## 使用(Use)
+> 注意：
+如没有添加meta[name=viewport]引入库后，会自动添加。
+
+默认以设计稿宽度 750px 为基础。
+```
+<script src="./node_modules/u-rem.js/u-rem.js" data-design="750"></script>
+```
+#### 修改设计稿宽度
+可以通过给script标签添加data-design属性修改设计稿宽度。
+
+
+
+
+
+
+
---
+++ u-rem.js
@@ -0,0 +1,30 @@
+/*
+	移动端布局rem
+	设置元素width height 采用rem
+*/
+(function(win,doc){
+	var docEl = doc.documentElement;
+	var scripts = doc.querySelector('script[data-design]');
+	var designWidth = scripts ? ~~(scripts.dataset.design):750;
+	
+	var fontEl = doc.createElement('style');
+	var dpr = 1;
+	var scale = 1 / dpr;
+	var rem = 100 * (docEl.clientWidth / designWidth);
+	if(!doc.querySelector('meta[name="viewport"]')){
+		var metaEl = document.createElement("meta");
+		metaEl.setAttribute('name','viewport');
+		// 设置viewport
+		metaEl.setAttribute('content', 'width=device-width, initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale +', user-scalable=no');
+		docEl.firstElementChild.appendChild(metaEl);
+	}
+	docEl.firstElementChild.appendChild(fontEl);
+	var resizeEvt = ('orientationchange' in window) ? 'orientationchange' : 'resize';
+	var recalc = function () {
+        rem = 100 * (docEl.clientWidth/designWidth);
+        fontEl.innerHTML = 'html{font-size:' + rem + 'px!important;}';
+    }
+    win.addEventListener(resizeEvt,recalc,false);
+    win.addEventListener('DOMContentLoaded',recalc, false);
+	
+})(window,document)
